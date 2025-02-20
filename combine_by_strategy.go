7e936d3efc484b63f6a45a41b8a69adbdc9da26d
Dima Gershovich <demitry.gershovich@gmail.com>
2025-02-20 22:11:38 +0200

    Added combine priority channels by strategy

---
+++ combine_by_strategy.go
@@ -0,0 +1,51 @@
+package priority_channels
+
+import (
+	"context"
+
+	"github.com/dmgrit/priority-channels/internal/selectable"
+)
+
+func CombineByStrategy[T any, W any](ctx context.Context,
+	strategy PrioritizationStrategy[W],
+	priorityChannelsWithWeight []PriorityChannelWithWeight[T, W],
+	options ...func(*PriorityChannelOptions)) (*PriorityChannel[T], error) {
+	channels := toSelectableChannelsWithWeight[T](priorityChannelsWithWeight)
+	return newByStrategy(ctx, strategy, channels, options...)
+}
+
+type PriorityChannelWithWeight[T any, W any] struct {
+	name            string
+	priorityChannel *PriorityChannel[T]
+	weight          W
+}
+
+func (c *PriorityChannelWithWeight[T, W]) Name() string {
+	return c.name
+}
+
+func (c *PriorityChannelWithWeight[T, W]) PriorityChannel() *PriorityChannel[T] {
+	return c.priorityChannel
+}
+
+func (c *PriorityChannelWithWeight[T, W]) Weight() W {
+	return c.weight
+}
+
+func NewPriorityChannelWithWeight[T any, W any](name string, priorityChannel *PriorityChannel[T], weight W) PriorityChannelWithWeight[T, W] {
+	return PriorityChannelWithWeight[T, W]{
+		name:            name,
+		priorityChannel: priorityChannel,
+		weight:          weight,
+	}
+}
+
+func toSelectableChannelsWithWeight[T any, W any](
+	priorityChannelsWithWeight []PriorityChannelWithWeight[T, W]) []selectable.ChannelWithWeight[T, W] {
+	res := make([]selectable.ChannelWithWeight[T, W], 0, len(priorityChannelsWithWeight))
+	for _, q := range priorityChannelsWithWeight {
+		priorityChannel := q.PriorityChannel()
+		res = append(res, asSelectableChannelWithWeight(priorityChannel, q.Name(), q.Weight()))
+	}
+	return res
+}
